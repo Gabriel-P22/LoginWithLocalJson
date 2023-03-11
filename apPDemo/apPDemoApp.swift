8ad328caa8438c810b8febe973d82e3216d70f99
gpaschoal <Gabriel.Paschoal-ext@ab-Inbev.com>
2023-03-11 16:31:57 -0300

    Initial Commit

---
+++ apPDemo/apPDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  apPDemoApp.swift
+//  apPDemo
+//
+//  Created by Gabriel Paschoal on 11/03/23.
+//
+
+import SwiftUI
+
+@main
+struct apPDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
